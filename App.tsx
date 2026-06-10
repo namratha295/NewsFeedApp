d350021c36d9c5ec570cbdeeef4060f7bf99e8b9
namratha.arun <namratha.arun@yuccasolutions.com>
2026-06-10 15:57:50 +0530

    feat: initialize NewsFeedApp with essential configurations
    - Add package.json with dependencies and scripts for React Native development
    - Create tailwind.config.js for Tailwind CSS integration
    - Set up tsconfig.json for TypeScript support with React Native

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import "./global.css";
+
+const App = () => {
+  return (
+    <View className="flex-1 bg-slate-50 px-4">
+      <Text className="text-xl font-semibold text-slate-900">News Feed App</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
